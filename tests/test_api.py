f740806d530a737d6929e5171ad3b168ca14fe57
Vsevolod Gruntovsky <seva.ru2001@yandex.ru>
2025-08-07 09:50:44 +0300

    научился делать unit тест и интеграционный тест

---
+++ tests/test_api.py
@@ -0,0 +1,30 @@
+import pytest
+from httpx import AsyncClient, ASGITransport
+from lesson7 import app
+
+
+@pytest.mark.asyncio
+async def test_get_books():
+    async  with AsyncClient(transport=ASGITransport(app=app),
+                            base_url="http://test",
+                            ) as ac:
+        response = await ac.get("/books")
+        assert response.status_code == 200
+
+        data = response.json()
+        assert len(data) == 2
+
+
+@pytest.mark.asyncio
+async def test_post_books():
+    async  with AsyncClient(transport=ASGITransport(app=app),
+                            base_url="http://test",
+                            ) as ac:
+        response = await ac.post("/books", json={
+            "title": "name_book",
+            "author": "name_author",
+        })
+        assert response.status_code == 200
+
+        data = response.json()
+        assert data == {"success": True, "message": "Книга добавлена"}
