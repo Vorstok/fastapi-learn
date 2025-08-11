984419dcdf04bde40353b0a285d771d0bf7f097e
Vsevolod Gruntovsky <gruntovskii@sfedu.ru>
2025-08-11 11:25:32 +0300

    добавил базовый main из прошлого урока и немного отредактировал его

---
+++ 12_project_structure/main.py
@@ -0,0 +1,71 @@
+from typing import Annotated
+
+from fastapi import FastAPI, Depends
+from pydantic import BaseModel
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+app = FastAPI()
+engine = create_async_engine('sqlite+aiosqlite:///books.db')
+
+new_session = async_sessionmaker(engine, expire_on_commit=False)
+
+
+async def get_session():
+    async with new_session() as session:
+        yield session
+
+
+SessionDep = Annotated[AsyncSession, Depends(get_session)]
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class BookModel(Base):
+    __tablename__ = "books"  # Исправлено: двойные подчеркивания вместо жирного текста
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    author: Mapped[str]
+    title: Mapped[str]
+
+
+class BookSchema(BaseModel):
+    title: str
+    author: str
+
+
+class BookGetSchema(BaseModel):
+    id: int
+    title: str
+    author: str
+
+
+@app.post("/setup_database")
+async def setup_database():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+
+@app.post("/books")
+async def add_book(book: BookSchema, session: SessionDep):
+    new_book = BookModel(
+        title=book.title,
+        author=book.author,
+    )
+    session.add(new_book)
+    await session.commit()
+    return book
+
+
+@app.get("/books")
+async def get_books(session: SessionDep) -> list[BookGetSchema]:
+    query = select(BookModel)
+    result = await session.execute(query)
+    books = result.scalars().all()
+    print(f"{books=}")
+    return books
